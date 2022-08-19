0dc8b9906b96b21aea38c6e56d22ec7b7d956e2a
Cailjones <57351045+Cailjones@users.noreply.github.com>
2022-08-19 23:55:41 +0100

    yes no qustion

---
+++ RobsGame_BlackJack/RobsGame_BlackJack/RobsGame_BlackJack.cpp
@@ -0,0 +1,31 @@
+// RobsGame_BlackJack.cpp : This file contains the 'main' function. Program execution begins and ends there.
+//
+
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    string NameOfPlayer,yesNo;
+    cout << " =============================== \n";
+    cout << " ==== Welcome to Black Jack ==== \n";
+    cout << " =============================== \n";
+
+    cout << "What is your name ?\n";
+    cin >> NameOfPlayer;
+
+    cout << "is this your name ?\n " << NameOfPlayer;
+    cout << "Do you want more tea?";
+
+    cin >> yesNo;
+    if (yesNo == 'Y')
+    {
+
+    }
+    else if (yesNo == 'N')
+    {
+
+    }
+
+}
+
