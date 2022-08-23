d730d4773f5abb87e04fb32e85bc8332e4cc13ee
Cailjones <57351045+Cailjones@users.noreply.github.com>
2022-08-23 20:07:24 +0100

    QOL changes

--- RobsGame_BlackJack/RobsGame_BlackJack/RobsGame_BlackJack.cpp
+++ RobsGame_BlackJack/RobsGame_BlackJack/RobsGame_BlackJack.cpp
@@ -51,14 +51,14 @@ int main()
     Rules();
 
     //dysplays name of player
-    cout << "welcome " << NameOfPlayer << " lets begine\n";
+    cout << "Welcome " << NameOfPlayer << " lets begine.\n";
     cout << "\n";
 
     //Calculationg player start numbers
     Playertotal = rngNumb1 + rngNumb2;
     cout << "Here your first 2 numbers\n";
-    cout << rngNumb1 << ", " << rngNumb2 << " This is your numbers";
-    cout << "your total is" << Playertotal << "\n";
+    cout << rngNumb1 << ", " << rngNumb2 << " This is your numbers.";
+    cout << "Your total is" << Playertotal << "\n";
 
     //do loop containg if statments so if the player was to go over a spasific value then it will do as follows
     // 1. over 21 will make the player bust and lose and show delers value
@@ -70,26 +70,26 @@ int main()
         if (Playertotal > 21)
         {
             cout << "You are bust you have lost against the dealer!!\n";
-            cout << "His total was :" << dealersNumber << "\n";
+            cout << "His total was : " << dealersNumber << "\n";
             break;
         }
         //2.
         else if (Playertotal == 21)
         {
 
-            cout << "congatulations you have a total of 21 the best score you won against the dealer!!\n";
+            cout << "Congatulations you have a total of 21 the best score you won against the dealer!!\n";
             break;
         }
         //3.
         else 
         {
-            cout << "do you want anotehr card? (y/n)\n";
+            cout << "Do you want anotehr card? (y/n)\n";
             cin >> StickOrTwist;
 
             if (StickOrTwist == "y")
             {
                 Playertotal = Playertotal + rngNumb3;
-                cout << "your total is now : " << Playertotal << "\n";
+                cout << "Your total is now : " << Playertotal << "\n";
             }
         }
 
@@ -101,19 +101,19 @@ int main()
     if (StickOrTwist == "n")
     {
         //gives the 2 total dealers and player
-        cout << "your Total was :\n " << Playertotal;
+        cout << "Your Total was :\n " << Playertotal;
         cout << "The dealers totasl was :\n " << dealersNumber;
 
         // if the players total id grater than the dealers then the player wins
         if (Playertotal >= dealersNumber)
         {
-            cout << "You win against the dealer congatulatuions!!\n";
+            cout << "You win against the dealer congatulatuions!!\n ";
         }
 
         //otherwise the player loses
         else
         {
-            cout << "you have lost against the dealer unlucky maybe next time.\n";
+            cout << "You have lost against the dealer unlucky maybe next time.\n ";
             
         }
  
